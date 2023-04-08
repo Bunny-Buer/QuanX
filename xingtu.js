975ec959243f578eac702bac4a0fefa55951a0ab
Bunny-Buer <129875279+Bunny-Buer@users.noreply.github.com>
2023-04-09 02:58:47 +0800

    Update xingtu.js

--- xingtu.js
+++ xingtu.js
@@ -3,34 +3,17 @@
 版本：xxx
 使用说明：此脚本仅供学习，请勿传播，因个人传播可能造成后果的概不负责
 [rewrite_local]
-https://(commerce-.*api|pay).(faceu|wecut).(com|mobi)/(commerce|apple)/(iosAppVerifyReceipt.php|v1/subscription/user_info) url script-response-body https://ghproxy.com/https://raw.githubusercontent.com/Bunny-Buer/QuanX/Script/xingtu.js
+^https?:\/\/commerce-api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info* url script-response-body https://ghproxy.com/https://raw.githubusercontent.com/Bunny-Buer/QuanX/Script/xingtu.js
 [mitm] 
-hostname=commerce-i18n-api.faceu.mobi,commerce-api.faceu.mobi, pay.wecut.com
+hostname=*.faceu.mobi
 ****************/
-const path1 = "/commerce/v1/subscription/user_info";
-const path2 = "/apple/iosAppVerifyReceipt.php";
-
-let ddgksf2013 = JSON.parse($response.body);
-
-if ($request.url.indexOf(path1) != -1)
-{
-     if(ddgksf2013.response){
-           var tmp = JSON.parse(ddgksf2013.response);
-           tmp.start_time = 1584674770;
-           tmp.end_time = 4077660370;
-           tmp.is_cancel_subscribe = false;
-           tmp.subscribe_type = "auto";
-           tmp.flag = true;
-           ddgksf2013.response =  JSON.stringify(tmp);  
-     }
-    ddgksf2013.data.start_time = 1584674770;
-    ddgksf2013.data.end_time = 4077660370;
-    ddgksf2013.data.is_cancel_subscribe = false;
-    ddgksf2013.data.subscribe_type = "auto";
-    ddgksf2013.data.flag = true;
+let obj = JSON.parse($response.body);
+ obj.data.flag = true;
+ obj.data.end_time = 8092599349;
+ obj.data.vip_real_end = 8092599349;
+obj.data.is_first_subscribe = true;
+obj.data.is_cancel_subscribe = false;
+ $done({
+ body: JSON.stringify(obj)
 }
-if ($request.url.indexOf(path2) != -1)
-{
-   ddgksf2013.data = {"isValid": 1,"expiresTs": 4077660370}
-}
-$done({body: JSON.stringify(ddgksf2013)});
+);
