6c03d74f4893b90cca053d106790cd9366719b2f
Bunny-Buer <129875279+Bunny-Buer@users.noreply.github.com>
2023-07-28 11:07:04 +0800

    Xmind.js

--- Xmind.js
+++ Xmind.js
@@ -1,20 +1,19 @@
 /****************
-更新时间：2023-4-07
+更新时间：2023-7-28
 软件版本：v22.111
 使用说明：此脚本仅供学习，请勿传播，因个人传播可能造成后果的概不负责
 [rewrite_local]
 ^https:\/\/www\.xmind\.app\/\_res\/devices url script-response-body https://raw.githubusercontent.com/Bunny-Buer/QuanX/Script/Xmind.js
 [mitm] 
-hostname=www.xmind.app, www.xmind.net
+hostname = www.xmind.*
 ****************/
+chxm1023 = {
+  "license" : {
+    "status" : "sub",
+    "expireTime" : 4102415999000
+  },
+  "_code" : 200,
+  "raw_data" : "bKnTeMMu/TJ86ZFoUqG+sfKC1nitD8b/NaUT93UBc0woo0BF5uBVYXsHUQJkQxLHGlZCbdnKaKzZ4iljM9xddkyMycpUbAtDoywnr4RNzxqXY20pDM0Bdgm+HssaS1RworH8aaaf0IYnnGplxIuqMyT9107PrenF0MBUIaKU9Cw="
+};
 
-var obj = JSON.parse($response.body);
-obj = {
-   "raw_data": "DBcBHgojrvPgruIJMfb4KK/76CmjxSHSp9KipRwOnQUuf+Gt2FncFUzNvxZydUeXEzDZt/XWEm91lHFYrvT0f2AFap2L4psLI/34sKU9VLGH7kFsBYlexU/nifBtosMJqQxL4TU1pjvjl+1uyWsFAeGR42aEnVhQWgvJB5Ovcd0=",
-   "license": {
-     "status": "sub",
-     "expireTime": 4102415999000
-   },
-   "_code": 200
- }
-$done({body: JSON.stringify(obj)});
+$done({body : JSON.stringify(chxm1023)});
